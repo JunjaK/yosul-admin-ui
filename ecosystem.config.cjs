98bc9637ccea1eeaa151588a43c4450faf2592dd
Junjak <haring157@naver.com>
2025-05-14 00:55:52 +0900

    ci/cd 추가

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   // 배포 대상에 대한 설정
   apps: [
     {
-      name: 'yamyam-admin',
+      name: 'yosul-admin',
       port: '4000',
       exec_mode: 'cluster',
       instances: 'max',
